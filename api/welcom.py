c5a95c2a87e88ff807d3b718c872a53e13a81f8b
dinashboul <dinaalshboul@gmail.com>
2022-10-31 15:44:19 +0000

    feat:add new welcome message

--- api/welcom.py
+++ api/welcom.py
@@ -1,12 +1,12 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        
-        self.wfile.write(str(date).encode())
+
+        message="welcome howdy"
+        self.wfile.write(message.encode())
+        return
