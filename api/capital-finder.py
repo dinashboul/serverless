4584e9c5ca1454148abb55554eafe3b024c5517d
dinashboul <dinaalshboul@gmail.com>
2022-10-31 18:34:32 +0000

    fix the functionality

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -2,37 +2,34 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        url_path = self.path
-        url_components = parse.urlsplit(url_path)
-        query_list = parse.parse_qsl(url_components.query)
-        dictionary = dict(query_list)
-
-        if 'name' in dictionary:
-            query = dictionary['name']
+        s = self.path
+        url_components = parse.urlsplit(s)
+        print(url_components)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        if 'country' in dic:
+            country = dic['country']
             url = 'https://restcountries.com/v3.1/name/'
-            base_url = url + query
-            response = requests.get(base_url)
-            data = response.json()
+            r = requests.get(url + country)
+            data = r.json()
             capital = data[0]['capital'][0]
-            message = f"The capital of {query} is {capital}"
-
-        elif 'name' in dictionary:
-            url = 'https://restcountries.com/v3.1/capital/'  
-            capital = dictionary["name"]
-            response = requests.get(url + capital )
-            data = response.json()
+            message = f"The capital of {dic['country']} is {capital}"
+        elif 'capital' in dic:
+            capital = dic['capital']
+            url ='https://restcountries.com/v3.1/capital/'
+            r = requests.get(url+capital)
+            data = r.json()
+            print(data)
             country = data[0]['name']['common']
-            # capital = str(country["name"])
-            message = f"The {country} is capital of {capital}"
-
+            message = f"{dic['capital']} is the capital of {country}"
         else:
-            message = "Please enter another country or another capital"
+            message = "Please enter a country name or a capital name"
+
 
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(message.encode())
 
