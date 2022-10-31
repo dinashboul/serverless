f78ec6334f6f63835bc41df4d9651159f5e3b398
dinashboul <dinaalshboul@gmail.com>
2022-10-31 19:05:13 +0000

    fix : parsing the project

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -4,28 +4,22 @@ import requests
 
 class handler(BaseHTTPRequestHandler):
      def do_GET(self):
-        s = self.path
-        url_components = parse.urlsplit(s)
-        print(url_components)
-        query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
-        if 'country' in dic:
-            country = dic['country']
+        url_path = self.path
+        url_components = parse.urlsplit(url_path)
+        query_list = parse.parse_qsl(url_components.query)
+        dictionary  = dict(query_list)
+
+
+        if 'country' in dictionary :
+            country = dictionary ['country']
             url = 'https://restcountries.com/v3.1/name/'
-            r = requests.get(url + country)
-            data = r.json()
+            response  = requests.get(url + country)
+            data = response.json()
             capital = data[0]['capital'][0]
-            message = f"The capital of {dic['country']} is {capital}"
-        elif 'capital' in dic:
-            capital = dic['capital']
-            url ='https://restcountries.com/v3.1/capital/'
-            r = requests.get(url+capital)
-            data = r.json()
-            print(data)
-            country = data[0]['name']['common']
-            message = f"{dic['capital']} is the capital of {country}"
+            message = f"The capital of {country} is {capital}"
+
         else:
-            message = "Please enter a country name or a capital name"
+            message = "Please provide me with a correct name"
 
 
         self.send_response(200)
